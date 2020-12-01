9d05d0bdde15d773296edaca62a0cfeb02cd988e
Barb H <60275319+barbh1307@users.noreply.github.com>
2020-12-01 11:38:12 -0600

    change usernames

--- sql/insert4_m_user.sql
+++ sql/insert4_m_user.sql
@@ -3,7 +3,7 @@
 --
 
 INSERT INTO `m_user` (`id_master_user`, `k_username`, `userpassword`, `fk_muser_rolecode_muserrole`) VALUES
-(1, 'JulieB', '', 2),
-(2, 'BonnieG', '', 1),
-(3, 'CoreyB', '', 3),
-(4, 'BarbH', '', 4);
+(1, 'JB', '', 2),
+(2, 'BG', '', 1),
+(3, 'CB', '', 3),
+(4, 'BH', '', 4);
